3850140d6e3196f6af25ce25831a7e41b043a41e
roaldl <roald.lemmen@ouce.ox.ac.uk>
2018-11-07 15:32:49 +0000

    Adjust hue colours

--- photobooth/hue.py
+++ photobooth/hue.py
@@ -4,17 +4,21 @@ import random
 class HueController:
 
     def __init__(self):
-        self.bridge = Bridge() # Enter bridge IP here.
+        self.bridge = Bridge('192.168.1.107') # Enter bridge IP here.
         #If running for the first time, press button on bridge and run with b.connect() uncommented
         #bridge.connect()
         self.lights = self.bridge.get_light_objects()
+        
+        for light in self.lights:
+            print(light.brightness)
+            print(light.xy)
 
     def set_for_photo(self):
         for light in self.lights:
             light.brightness = 254
-            light.xy = [random.random(),random.random()]
+            light.xy = [0.4578, 0.41]
 
     def set_for_wait(self):
         for light in self.lights:
-            light.brightness = 0
+            light.brightness = 1
             light.xy = [random.random(),random.random()]
